18e03de0cf0b58f78ba22280e7dcb4b6fb44abf3
Hassan <mughalzbrother@gmail.com>
2023-01-15 13:12:36 -0700

    first commit

---
+++ app.ts
@@ -0,0 +1,24 @@
+import inquirer from "inquirer";
+
+type ansType ={
+    userGuess:number 
+}
+
+const systemGeneratedNo =  Math.floor(Math.random() *10 );
+
+const answers = await inquirer.prompt([
+    {
+        type:"number",
+        name:"userGuess",
+        message:"Write your guess: "
+    }
+])
+
+const {userGuess} = answers;
+console.log(userGuess,"userGuess",systemGeneratedNo,"Sys");
+
+if(userGuess===systemGeneratedNo){
+    console.log("Yeaaa Your answer is correct \n You Win"); 
+} else{
+    console.log("Please try again Better luck next time");
+}
